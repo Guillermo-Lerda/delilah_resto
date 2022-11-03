cafb2e38933b59e87f008fd873854440965309bd
Guillermo Lerda <glerda83@gmail.com>
2022-11-02 21:44:51 -0300

    db password

--- database_conection/conection.js
+++ database_conection/conection.js
@@ -4,8 +4,7 @@ const DB_PORT = process.env.DB_PORT;
 const DB_DATABASE = process.env.DB_DATABASE; 
 const HOST_DB =  process.env.HOST_DB;
 const USER_DB = process.env.USER_DB;
-const PASS_DB = process.env.PASS_DB;
-const path = `mysql://${USER_DB}:${PASS_DB}@${HOST_DB}:${DB_PORT}/${DB_DATABASE}`;
+const path = `mysql://${USER_DB}@${HOST_DB}:${DB_PORT}/${DB_DATABASE}`;
 const sequelize = new Sequelize(path,{operatorsAliases: 0 });
 sequelize.authenticate()
 .then(()=>{
